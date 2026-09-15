7837097f179e6e9a07f1682d06e33b20fc0d56aa
error-898-15 <cojni34@gmail.com>
2026-09-15 20:12:44 +0600

    Update build.gradle.kts

--- UchihaPlugin/build.gradle.kts
+++ UchihaPlugin/build.gradle.kts
@@ -1,16 +1,10 @@
 version = 1
 
 cloudstream {
-    description = "Uchiharepo CloudStream CS3 Foundation Plugin"
+    description = "Uchiharepo - Blakite Anime (Hindi & English Sub/Dub) CloudStream Extension"
     authors = listOf("error-898-15")
     status = 1
-    tvTypes = listOf("Movie", "TvSeries", "Anime")
-    language = "en"
+    tvTypes = listOf("Anime", "Movie", "Cartoon")
+    language = "hi"
     iconUrl = "https://raw.githubusercontent.com/error-898-15/Uchiharepo/master/icon.png"
-}
-
-android {
-    defaultConfig {
-        minSdk = 21
-    }
 }
