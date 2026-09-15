5c2971d786fdd36283d37a69d00438deb4d442e8
error-898-15 <cojni34@gmail.com>
2026-09-15 19:23:45 +0600

    Create build.gradle.kts

---
+++ UchihaPlugin/build.gradle.kts
@@ -0,0 +1,16 @@
+version = 1
+
+cloudstream {
+    description = "Uchiharepo CloudStream CS3 Foundation Plugin"
+    authors = listOf("error-898-15")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries", "Anime")
+    language = "en"
+    iconUrl = "https://raw.githubusercontent.com/error-898-15/Uchiharepo/master/icon.png"
+}
+
+android {
+    defaultConfig {
+        minSdk = 21
+    }
+}
