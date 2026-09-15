7f17facd8779e1a0654f9edebb81398920a3d9d7
error-898-15 <cojni34@gmail.com>
2026-09-15 20:16:54 +0600

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,8 @@
 rootProject.name = "Uchiharepo"
 
-// Automatically include subdirectories containing build.gradle.kts
-val disabled = listOf<String>()
-
-File(rootDir, ".").eachDir { dir ->
-    if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
+// Automatically include subprojects that have build.gradle.kts
+File(rootDir, ".").listFiles()?.filter { it.isDirectory }?.forEach { dir ->
+    if (File(dir, "build.gradle.kts").exists()) {
         include(dir.name)
     }
-}
-
-fun File.eachDir(block: (File) -> Unit) {
-    listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
 }
