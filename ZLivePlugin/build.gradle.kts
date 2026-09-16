0eeefdc13cbb5d235dae07397e95969a4cd90f64
error-898-15 <cojni34@gmail.com>
2026-09-16 09:46:45 +0600

    Create build.gradle.kts

---
+++ ZLivePlugin/build.gradle.kts
@@ -0,0 +1,16 @@
+version = 1
+
+cloudstream {
+    description = "ZLive - Watch Live Sports, Events & Worldwide TV Channels Free"
+    authors = listOf("error-898-15")
+    status = 1
+    tvTypes = listOf("Live")
+    language = "en"
+    iconUrl = "https://raw.githubusercontent.com/error-898-15/Uchiharepo/main/icon.png"
+}
+
+android {
+    defaultConfig {
+        minSdk = 21
+    }
+}
