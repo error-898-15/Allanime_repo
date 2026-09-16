b1ddb285169b751eca99bc25d424270df5219616
error-898-15 <cojni34@gmail.com>
2026-09-16 16:38:23 +0600

    Update build.gradle.kts

--- FojikPlugin/build.gradle.kts
+++ FojikPlugin/build.gradle.kts
@@ -1,16 +1,5 @@
-version = 1
-
 cloudstream {
-    description = "Fojik / MLWBD - Watch & Download Hindi, Dual Audio, Bengali, Bollywood & Hollywood Movies and Web Series in 480p, 720p, 1080p and 4K UHD"
+    setRepo(System.getenv("GITHUB_REPOSITORY") ?: "error-898-15/Uchiharepo")
     authors = listOf("error-898-15")
     status = 1
-    tvTypes = listOf("Movie", "TvSeries", "Anime")
-    language = "bn"
-    iconUrl = "https://fojik.site/wp-content/uploads/Untitled2.png"
-}
-
-android {
-    defaultConfig {
-        minSdk = 21
-    }
 }
