9bc1997f89eb263547f7e413640ee4354220c635
error-898-15 <cojni34@gmail.com>
2026-09-16 16:24:46 +0600

    Update build.gradle.kts

--- FojikPlugin/build.gradle.kts
+++ FojikPlugin/build.gradle.kts
@@ -1,35 +1,16 @@
-plugins {
-    id("com.android.library")
-    id("kotlin-android")
+version = 1
+
+cloudstream {
+    description = "Fojik / MLWBD - Watch & Download Hindi, Dual Audio, Bengali, Bollywood & Hollywood Movies and Web Series in 480p, 720p, 1080p and 4K UHD"
+    authors = listOf("error-898-15")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries", "Anime")
+    language = "bn"
+    iconUrl = "https://fojik.site/wp-content/uploads/Untitled2.png"
 }
 
 android {
-    namespace = "com.uchiharepo.fojik"
-    compileSdk = 34
-
     defaultConfig {
         minSdk = 21
     }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    kotlinOptions {
-        jvmTarget = "1.8"
-        freeCompilerArgs = freeCompilerArgs + "-Xopt-in=kotlin.RequiresOptIn"
-    }
-}
-
-cloudstream {
-    setRepo(System.getenv("GITHUB_REPOSITORY") ?: "error-898-15/Uchiharepo")
-    authors = listOf("error-898-15")
-    status = 1
-}
-
-dependencies {
-    val cloudstreamApiVersion = "pre-release"
-    implementation("com.github.recloudstream:cloudstream:$cloudstreamApiVersion")
-    implementation("org.jsoup:jsoup:1.17.2")
 }
