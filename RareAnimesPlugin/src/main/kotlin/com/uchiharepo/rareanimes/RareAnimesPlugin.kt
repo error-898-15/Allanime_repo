6deba4648110f680a33afb61921bbd96ba0688d3
error-898-15 <cojni34@gmail.com>
2026-09-17 08:50:55 +0600

    Create RareAnimesPlugin.kt

---
+++ RareAnimesPlugin/src/main/kotlin/com/uchiharepo/rareanimes/RareAnimesPlugin.kt
@@ -0,0 +1,12 @@
+package com.uchiharepo.rareanimes
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class RareAnimesPlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(RareAnimesProvider())
+    }
+}
