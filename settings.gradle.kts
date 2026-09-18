ca3f3ba1135aeea39c1e0d6d757b50f14bbeb78a
error-898-15 <cojni34@gmail.com>
2026-09-18 10:19:03 +0600

    Add RareAnimesPlugin to project includes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "Uchiharepo"
-
 include(":BlakitePlugin")
 include(":ZLivePlugin")
 include(":AnimeDekhoPlugin")
 include(":AnimeSaltPlugin")
 include(":MLWBDPlugin")
+include(":RareAnimesPlugin")
