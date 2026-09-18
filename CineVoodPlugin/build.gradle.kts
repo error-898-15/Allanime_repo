49aec7f3b43cb4036ed0b46fa3d69acd5f69b4eb
error-898-15 <cojni34@gmail.com>
2026-09-18 15:05:52 +0600

    Add initial build.gradle.kts for CineVoodPlugin

---
+++ CineVoodPlugin/build.gradle.kts
@@ -0,0 +1,16 @@
+version = 1
+
+cloudstream {
+    description = "CineVood - Watch & Download Bollywood, Hollywood, South Hindi Dubbed Movies & Web Series with Original High-Speed Servers"
+    authors = listOf("error-898-15")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries", "AsianDrama")
+    language = "hi"
+    iconUrl = "https://cinevood.rocks/wp-content/uploads/2020/07/favicon2.png"
+}
+
+android {
+    defaultConfig {
+        minSdk = 21
+    }
+}
