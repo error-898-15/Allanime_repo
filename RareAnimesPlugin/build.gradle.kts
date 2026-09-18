f6f617f04ff5c3940fd9c33d0477cf1201ac13a6
error-898-15 <cojni34@gmail.com>
2026-09-18 10:23:39 +0600

    Add build.gradle.kts for RareAnimesPlugin

---
+++ RareAnimesPlugin/build.gradle.kts
@@ -0,0 +1,16 @@
+version = 1
+
+cloudstream {
+    description = "RareAnimes - Watch Hindi, Tamil, Telugu Dubbed Anime, Cartoons & Movies with StreamBeta, Mega & MultiQuality CDN"
+    authors = listOf("error-898-15")
+    status = 1
+    tvTypes = listOf("Anime", "AnimeMovie", "Cartoon", "Movie", "TvSeries")
+    language = "hi"
+    iconUrl = "https://www.rareanimes.mov/wp-content/uploads/2023/11/cropped-Rare-Animes-India-192x192.png"
+}
+
+android {
+    defaultConfig {
+        minSdk = 21
+    }
+}
