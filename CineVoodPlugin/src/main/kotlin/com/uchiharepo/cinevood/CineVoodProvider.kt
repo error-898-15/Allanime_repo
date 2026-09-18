eca5fbe6c6d2afa649bc4ccf07104af40fa4c2b1
error-898-15 <cojni34@gmail.com>
2026-09-18 22:30:34 +0600

    Add toJson import in CineVoodProvider.kt

--- CineVoodPlugin/src/main/kotlin/com/uchiharepo/cinevood/CineVoodProvider.kt
+++ CineVoodPlugin/src/main/kotlin/com/uchiharepo/cinevood/CineVoodProvider.kt
@@ -4,6 +4,7 @@ import android.util.Base64
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.AppUtils.parseJson
+import com.lagradost.cloudstream3.utils.AppUtils.toJson
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.ExtractorLinkType
 import com.lagradost.cloudstream3.utils.Qualities
@@ -32,7 +33,6 @@ class CineVoodProvider : MainAPI() {
         private const val USER_AGENT =
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
         private const val BYPASS_REFERER = "https://cinevood.rocks/"
-
         private val MIRRORS = listOf(
             "https://cinevood.rocks",
             "https://cinevood.lat",
