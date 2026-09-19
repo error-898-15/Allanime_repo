6647fd7db788df66413d848a5b2e9bc568222366
error-898-15 <cojni34@gmail.com>
2026-09-19 08:01:29 +0600

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,9 +2,5 @@ rootProject.name = "Uchiharepo"
 
 include(":BlakitePlugin")
 include(":ZLivePlugin")
-include(":AnimeDekhoPlugin")
 include(":AnimeSaltPlugin")
-include(":MLWBDPlugin")
 include(":RareAnimesPlugin")
-include(":CineVoodPlugin")
-include(":FlixVisionPlugin")
