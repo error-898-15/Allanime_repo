c3f5f6a362526672c5337149222c1edb4aaa3e4f
error-898-15 <cojni34@gmail.com>
2026-09-19 17:01:38 +0600

    Add build.gradle.kts for AniflixPlugin
    Initial setup for AniflixPlugin with configuration details.

---
+++ AniflixPlugin/build.gradle.kts
@@ -0,0 +1,16 @@
+version = 1
+
+cloudstream {
+    description = "Aniflix - Watch Anime Online Free with Multi-Server HLS & Dub Streams (Anixo, MegaVid, DesiDub, Filemoon, VidNest)"
+    authors = listOf("error-898-15")
+    status = 1
+    tvTypes = listOf("Anime", "AnimeMovie", "Cartoon")
+    language = "en"
+    iconUrl = "https://aniflix.uno/android/launchericon-192x192.png"
+}
+
+android {
+    defaultConfig {
+        minSdk = 21
+    }
+}
