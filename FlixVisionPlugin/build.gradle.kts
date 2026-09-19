66925d92a44e4e16e1b90b6c1237b2b67d0e9602
error-898-15 <cojni34@gmail.com>
2026-09-19 12:15:41 +0600

    Add initial build configuration for FlixVisionPlugin

---
+++ FlixVisionPlugin/build.gradle.kts
@@ -0,0 +1,16 @@
+version = 1
+
+cloudstream {
+    description = "FlixVision - Movies, TV Series & Multi-Server Direct Streams (FVSTREAM, VidSrc, SmashyStream, AutoEmbed)"
+    authors = listOf("error-898-15")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    language = "en"
+    iconUrl = "https://raw.githubusercontent.com/error-898-15/Uchiharepo/main/FlixVisionPlugin/icon.png"
+}
+
+android {
+    defaultConfig {
+        minSdk = 21
+    }
+}
