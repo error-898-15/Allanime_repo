a3696c9320682f32d47d31891a9b3a5ae0c3e871
error-898-15 <cojni34@gmail.com>
2026-09-20 12:20:19 +0600

    Add initial build.gradle.kts for GogoanimePlugin
    Initial setup for GogoanimePlugin with basic configuration.

---
+++ GogoanimePlugin/build.gradle.kts
@@ -0,0 +1,16 @@
+version = 1
+
+cloudstream {
+    description = "Gogoanime - Watch HD & Subbed Anime Online with Fast Original Servers"
+    authors = listOf("error-898-15")
+    status = 1
+    tvTypes = listOf("Anime", "AnimeMovie", "Movie", "TvSeries")
+    language = "en"
+    iconUrl = "https://i3.wp.com/gogoanime.by/wp-content/uploads/2024/05/cropped-favicon-2-192x192.png"
+}
+
+android {
+    defaultConfig {
+        minSdk = 21
+    }
+}
