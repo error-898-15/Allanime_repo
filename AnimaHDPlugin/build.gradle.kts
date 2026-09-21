3369d6ca06164b60598898b0e6ceb44c8ea4f0a8
error-898-15 <cojni34@gmail.com>
2026-09-21 10:25:08 +0600

    Initialize build.gradle.kts for AnimaHDPlugin

---
+++ AnimaHDPlugin/build.gradle.kts
@@ -0,0 +1,16 @@
+version = 1
+
+cloudstream {
+    description = "AnimaHD - Watch Hindi, Dual & Multi-Audio Anime & Movies with High-Speed Worker CDN & Google Drive"
+    authors = listOf("error-898-15")
+    status = 1
+    tvTypes = listOf("Anime", "AnimeMovie", "Cartoon", "Movie", "TvSeries")
+    language = "hi"
+    iconUrl = "https://animahd.com/wp-content/uploads/2026/06/e58b0fa5-1c81-4021-8a95-5c826587ba9b.png"
+}
+
+android {
+    defaultConfig {
+        minSdk = 21
+    }
+}
