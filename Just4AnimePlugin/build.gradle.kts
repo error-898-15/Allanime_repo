f99ac0296783767e9accc3e17570c47e0da084ea
error-898-15 <cojni34@gmail.com>
2026-09-21 19:39:42 +0600

    Create build.gradle.kts

---
+++ Just4AnimePlugin/build.gradle.kts
@@ -0,0 +1,16 @@
+version = 1
+
+cloudstream {
+    description = "Just4Anime - Watch Anime Online with Multi-Server High-Speed CDN & Sub/Dub"
+    authors = listOf("error-898-15")
+    status = 1
+    tvTypes = listOf("Anime", "AnimeMovie", "OVA")
+    language = "en"
+    iconUrl = "https://just4anime.online/apple-touch-icon.png"
+}
+
+android {
+    defaultConfig {
+        minSdk = 21
+    }
+}
