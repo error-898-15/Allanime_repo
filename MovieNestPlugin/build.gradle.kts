ab7076b0f7932f059b04d6fd136f48b3fd56d79f
error-898-15 <cojni34@gmail.com>
2026-09-21 08:35:48 +0600

    Create build.gradle.kts

---
+++ MovieNestPlugin/build.gradle.kts
@@ -0,0 +1,16 @@
+version = 1
+
+cloudstream {
+    description = "MovieNestBD - Watch Movies, TV Series, Anime & Bengali Content Online with High-Speed HLS Streaming"
+    authors = listOf("error-898-15")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries", "Anime", "AsianDrama")
+    language = "bn"
+    iconUrl = "https://movienestbd.best/favicon.png"
+}
+
+android {
+    defaultConfig {
+        minSdk = 21
+    }
+}
