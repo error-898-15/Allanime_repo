d6f6d8ae71f18c203fa63b677be2323ea14f27a1
error-898-15 <cojni34@gmail.com>
2026-09-22 10:21:22 +0600

    Initialize Just4AnimePlugin build configuration
    Added basic configuration for Just4Anime plugin.

---
+++ Just4AnimePlugin/build.gradle.kts
@@ -0,0 +1,16 @@
+version = 1
+
+cloudstream {
+    description = "Just4Anime - Stream HD Anime, Airing Episodes, Movies & Multi-Server Streams (Sub & Dub)"
+    authors = listOf("error-898-15")
+    status = 1
+    tvTypes = listOf("Anime", "AnimeMovie", "Movie", "TvSeries")
+    language = "en"
+    iconUrl = "https://just4anime.online/apple-touch-icon.png"
+}
+
+android {
+    defaultConfig {
+        minSdk = 21
+    }
+}
