adbce25ff05999094dbf453c9e05d67e171400f1
error-898-15 <cojni34@gmail.com>
2026-09-23 11:22:02 +0600

    Update build.gradle.kts

--- YoutubePlugin/build.gradle.kts
+++ YoutubePlugin/build.gradle.kts
@@ -4,7 +4,7 @@ cloudstream {
     description = "YouTube - Stream trending videos, music, gaming, news, and official channels directly on CloudStream"
     authors = listOf("error-898-15")
     status = 1
-    tvTypes = listOf("Others", "Live", "TvSeries")
+    tvTypes = listOf("Movie", "TvSeries", "Live")
     language = "en"
     iconUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/YouTube_full-color_icon_%282017%29.svg/512px-YouTube_full-color_icon_%282017%29.svg.png"
 }
