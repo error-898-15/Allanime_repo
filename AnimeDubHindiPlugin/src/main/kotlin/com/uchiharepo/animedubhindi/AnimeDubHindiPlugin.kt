b46c59941ea741bc4ab61069e72ed6b7d86317c1
error-898-15 <cojni34@gmail.com>
2026-09-23 17:03:14 +0600

    Create AnimeDubHindiPlugin.kt

---
+++ AnimeDubHindiPlugin/src/main/kotlin/com/uchiharepo/animedubhindi/AnimeDubHindiPlugin.kt
@@ -0,0 +1,12 @@
+package com.uchiharepo.animedubhindi
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class AnimeDubHindiPlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(AnimeDubHindiProvider())
+    }
+}
