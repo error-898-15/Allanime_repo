2138c83117237f5a54fb59519deacd952b9097b0
error-898-15 <cojni34@gmail.com>
2026-09-23 17:02:30 +0600

    Create build.gradle.kts

---
+++ AnimeDubHindiPlugin/build.gradle.kts
@@ -0,0 +1,16 @@
+version = 1
+
+cloudstream {
+    description = "AnimeDubHindi - Watch Hindi & Multi-Audio Anime & Movies with High-Speed Cloud CDN"
+    authors = listOf("error-898-15")
+    status = 1
+    tvTypes = listOf("Anime", "AnimeMovie", "Movie", "TvSeries")
+    language = "hi"
+    iconUrl = "https://www.animedubhindi.link/wp-content/uploads/2024/07/Untitled-design.png"
+}
+
+android {
+    defaultConfig {
+        minSdk = 21
+    }
+}
