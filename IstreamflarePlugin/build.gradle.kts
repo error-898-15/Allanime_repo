0a7d71bb0a884691ef11c47b5635c7ac63e7f551
error-898-15 <cojni34@gmail.com>
2026-09-24 18:09:12 +0600

    Add initial build configuration for IstreamflarePlugin

---
+++ IstreamflarePlugin/build.gradle.kts
@@ -0,0 +1,16 @@
+version = 1
+
+cloudstream {
+    description = "iStreamFlare - Stream Latest Movies & Web Series with Multi-Language Audio and High Quality DASH/HLS Playback"
+    authors = listOf("error-898-15")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    language = "hi"
+    iconUrl = "https://blogger.googleusercontent.com/img/a/AVvXsEgWJNM8v7dkKlHDuBncLOZsjiURJtbxv6de_W_TkIg75W51emlvr-3DATj02j__QUikkzjxhYKv8jYtQp4lc04xObvSTvthIHg_DA0Ud4SRiEUKqralljdfKnUumPN96NEBQwW6y0SpVKcCCPzuIwh8on5sgzjH7BT5PpR6_vp_qS7Qia8OMj04qz-DyMw=s937"
+}
+
+android {
+    defaultConfig {
+        minSdk = 21
+    }
+}
