ca1740a2a58c5a770ec659351b6058093c0d96d1
error-898-15 <cojni34@gmail.com>
2026-09-24 10:12:13 +0600

    Add AnimeHDPlugin metadata to update_plugins.py

--- .github/scripts/update_plugins.py
+++ .github/scripts/update_plugins.py
@@ -5,6 +5,10 @@ import os
 import hashlib
 
 PLUGIN_META = {
+    'AnimeHDPlugin': {
+        'name': 'AnimeHD',
+        'iconUrl': 'https://animahd.com/wp-content/uploads/2026/08/cropped-imageedit_4_6148516294-192x192.png'
+    },
     'BlakitePlugin': {
         'name': 'Blakite Anime',
         'iconUrl': 'https://blogger.googleusercontent.com/img/a/AVvXsEgWJNM8v7dkKlHDuBncLOZsjiURJtbxv6de_W_TkIg75W51emlvr-3DATj02j__QUikkzjxhYKv8jYtQp4lc04xObvSTvthIHg_DA0Ud4SRiEUKqralljdfKnUumPN96NEBQwW6y0SpVKcCCPzuIwh8on5sgzjH7BT5PpR6_vp_qS7Qia8OMj04qz-DyMw=s937'
